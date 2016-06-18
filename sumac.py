ccae3202ce94c12c47d03dbee53eeafb8fa77e5e
Dylan Hart <h@xor.li>
2016-06-18 16:34:44 -0500

    Restored functionality following Skype's new username validation system.
    Username can no longer be sent in the URL, has to be a POST request with the username in the form data. They also moved the validator location slightly. Response codes are no longer just "406" and "200", now they're a range of 5-number responses. "20000" is "OK" and "40601" is "Registration validation errors", a.k.a. "Not available".
    
    Thank you to @villamarinella for pointing this out in #acb1932!

--- sumac.py
+++ sumac.py
@@ -26,10 +26,9 @@ except ImportError:
   print "No stem!"
   print "try 'pip install stem'"
   print "Dependencies are listed in SUMAC's readme on Github!"
-
 print "\nSUMAC - Skype Username Mass Availability Checker  Copyright (C) 2015  Dylan Hart"
 print "sumac.py comes with ABSOLUTELY NO WARRANTY, to the extent permitted by applicable law."
-print "This is free software, and you are welcome to redistribute it under certain conditions.
+print "This is free software, and you are welcome to redistribute it under certain conditions."
 print "View the 'LICENSE' file included in the SUMAC GitHub repository for more information.\n"
 try:
     sys.argv[1]
@@ -52,28 +51,30 @@ else:
             open('available-skype.txt', 'w').close()
         print str(len(fcon_arr)) + " names loaded, and here we go!\n"
         for user in fcon_arr:
-            validator_url = "https://login.skype.com/json/validator?new_username=" + user
+            validator_url = "https://login.skype.com/registration/validator"
             validator_session = requests.session()
             validator_session.proxies = {'https': 'socks5://127.0.0.1:9050'}
-            validator_response = validator_session.get(validator_url)
+            validator_payload = "skypename="+user
+            validator_response = validator_session.post(validator_url, headers={"Origin":"https://login.skype.com","Accept":"application/json, text/javascript","User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"}, data=validator_payload)
             validator_resptxt = validator_response.text
             validator_json_decoded = json.loads(validator_resptxt)
             validator_status = validator_json_decoded['status']
-            if validator_status == 406:
-                print user + " is \033[91mtaken\033[0m [\033[91m" + str(validator_status) + "\033[0m]"
-            elif validator_status == 200:
-                print user + " is \033[92mavailable\033[0m [\033[92m" + str(validator_status) + "\033[0m]"
+            validator_status_code = validator_status['code']
+            if validator_status_code == 40601:
+                print user + " is \033[91mtaken\033[0m [\033[91m" + str(validator_status_code) + "\033[0m]"
+            elif validator_status_code == 20000:
+                print user + " is \033[92mavailable\033[0m [\033[92m" + str(validator_status_code) + "\033[0m]"
                 with open("available-skype.txt", "a") as availfile:
                     availfile.write(user + "\n")
                     availfile.close()
             else:
-                print user + " returned unknown status " + validator_status
+                print user + " returned unknown status"
             if not count % 4:
                 with Controller.from_port(port = 9051) as controller: # Tor control port is defined here
                     controller.authenticate()
                     controller.signal(Signal.NEWNYM)
                 time.sleep(9.2)
             count = count + 1
-            time.sleep(0.5)
+            time.sleep(0.3)
     except (KeyboardInterrupt, SystemExit):
         print "\nSUMAC exiting: I am terminated"
