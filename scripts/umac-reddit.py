3169986586b01fede0caead6ebf9ecfbdb515d84
Dylan Hart <h@xor.li>
2017-02-11 01:21:43 -0600

    Add a decent reddit checker
    This script relies on a .sh script which is also included here in the same folder; umac-reddit-verify.sh. That script is just a curl script to validate that "available" usernames are actually available. This script was rather simple to create, only problem was a 429 error from a non-browser user-agent, which I was disappointed to see Reddit using. Just in case anything else triggers it, I borrowed some headers from my browser to make it look good to the server when it's scanning. You'll notice the "(falsepos)" gets triggered a lot, and it takes a while to pass, that's simply because it's calling the "umac-reddit-verify.sh" script to verify the "available" username is actually good.

---
+++ scripts/umac-reddit.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+
+# Refer to LICENSE.txt in the Github repository to see how you can use this.
+# Copyright (c) Dylan Hart 2017
+
+import os, sys, random, subprocess
+
+try:
+  import time
+except ImportError:
+  print "No time! *sprints away*"
+try:
+  import json
+except ImportError:
+  print "No json!"
+try:
+  import requests
+except ImportError:
+  print "No requests!"
+
+print "\numac-reddit - Reddit Username Mass Availability Checker  Copyright (C) 2017  Dylan Hart"
+print "gumac.py comes with ABSOLUTELY NO WARRANTY, to the extent permitted by applicable law."
+print "This is free software, and you are welcome to redistribute it under certain conditions."
+print "View the 'LICENSE' file included in the umac-reddit GitHub repository for more information.\n"
+try:
+    sys.argv[1]
+except NameError:
+    print "You must specify a wordlist!"
+    print "Usage: " + sys.argv[0] + " <wordlist> [av|none]"
+except IndexError:
+    print "You must specify a wordlist!"
+    print "Usage: " + sys.argv[0] + " <wordlist> [av|none]"
+else:
+    try:
+        sys.argv[2]
+    except NameError:
+        silent = 'false'
+    except IndexError:
+        silent = 'false'
+    else:
+        silent = sys.argv[2]
+    if silent == 'false':
+        print "\033[92mGreen means available\033[0m"
+        print "\033[91mRed means unavailable\033[0m\n"
+    elif silent == 'av':
+        print "\033[92mGreen means available\033[0m\n"
+    elif silent == 'none':
+        pass
+    time.sleep(1)
+    try:
+        fname = sys.argv[1]
+        fh = open(fname)
+        fcon = fh.read()
+        count = 0
+        fcon_stripped = fcon.replace(" ", "")
+        fcon_stripped = fcon_stripped.replace("\r", "")
+        fcon_arr = fcon_stripped.rstrip().split("\n")
+        if not os.path.exists('available-reddit.txt'):
+            open('available-reddit.txt', 'w').close()
+        print str(len(fcon_arr)) + " names loaded, and here we go!\n"
+        if silent == "none":
+            print "Silent mode enabled, username output will only be given to file.\n"
+        elif silent == "av":
+            print "Semi-silent mode enabled, only available usernames will be shown.\n"
+            time.sleep(0.3)
+        for user in fcon_arr:
+            url = "https://www.reddit.com/user/" + user
+            session = requests.session()
+            headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                        'Accept-Encoding': 'gzip, deflate, sdch, br',
+                        'Accept-Language': 'en-US,en;q=0.8' }
+            response = session.get(url, headers=headers)
+            status = response.status_code
+            if int(status) == 200:
+                if silent == "av":
+                    pass
+                elif silent == 'false':
+                    print "\033[91m" + user + "\033[0m (" + str(status) + ")"
+            elif int(status) == 404:
+                verify = subprocess.Popen("sh $(pwd)/scripts/umac-reddit-verify.sh "+user, shell=True, stdout=subprocess.PIPE).stdout.read()
+                if verify.find("USERNAME_TAKEN") == -1:
+                    if silent == "none":
+                        pass
+                    else:
+                        print "\033[92m" + user + "\033[0m"
+                    with open("available-reddit.txt", "a") as af:
+                        af.write(user + "\n")
+                        af.close()
+                else:
+                    print "\033[91m" + user + "\033[0m (falsepos)"
+            else:
+                print user + " returned unknown status " + str(status)
+    except (KeyboardInterrupt, SystemExit):
+        print "\numac-reddit exiting: I am terminated"
